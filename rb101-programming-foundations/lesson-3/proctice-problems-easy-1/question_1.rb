fde37877e6f8966528d3ef631d78b9cef3e32a10
DumboCL <wyz513@gmail.com>
2020-03-04 16:40:58 +0800

    finished practice-problems-easy-1

---
+++ rb101-programming-foundations/lesson-3/proctice-problems-easy-1/question_1.rb
@@ -0,0 +1,9 @@
+numbers = [1, 2, 2, 3]
+numbers.uniq
+
+puts numbers
+
+# 1
+# 2
+# 3
+# 4
---
+++ rb101-programming-foundations/lesson-3/proctice-problems-easy-1/question_8.rb
@@ -0,0 +1,7 @@
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+array = []
+array << "Barney"
+array << flintstones["Barney"]
+
+p array
