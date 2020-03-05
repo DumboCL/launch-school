9fc5997d7aebe5837447a0e054846916f54ba59c
DumboCL <wyz513@gmail.com>
2020-03-05 16:14:33 +0800

    finished practice problems easy 2

---
+++ rb101-programming-foundations/lesson-3/practice-problems-easy-2/question_6.rb
@@ -0,0 +1,5 @@
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones << "Dino"
+
+p flintstones
---
+++ rb101-programming-foundations/lesson-3/practice-problems-easy-2/question_8.rb
@@ -0,0 +1,4 @@
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+puts advice.slice!("Few things in life are as important as ")
+puts advice
