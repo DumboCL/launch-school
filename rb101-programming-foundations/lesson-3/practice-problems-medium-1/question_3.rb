9c2a69e5a220d95bb7264c11f584462ad9766633
DumboCL <wyz513@gmail.com>
2020-03-06 15:48:14 +0800

    finished practice problems medium 1

---
+++ rb101-programming-foundations/lesson-3/practice-problems-medium-1/question_3.rb
@@ -0,0 +1,22 @@
+def factors_1(number)
+  divisor = number
+  factors = []
+  begin
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end until divisor == 0
+  factors
+end
+
+def factors_2(number)
+  divisor = number
+  factors = []
+  while divisor > 0
+    factors << number / divisor if number % divisor == 0
+    divisor -= 1
+  end
+  factors
+end
+
+p factors_1(50)
+p factors_2(50)
