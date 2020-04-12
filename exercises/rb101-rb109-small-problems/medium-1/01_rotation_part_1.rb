6d3a83e5ffd8a39f5b155113e70580fbf2ebdf19
DumboCL <wyz513@gmail.com>
2020-04-12 19:23:04 +0800

    started medium 1

---
+++ exercises/rb101-rb109-small-problems/medium-1/01_rotation_part_1.rb
@@ -0,0 +1,13 @@
+def rotate_array(arr)
+  new_arr = arr.dup
+  first = new_arr.shift
+  new_arr << first
+end
+
+puts rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+puts rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+puts rotate_array(['a']) == ['a']
+
+x = [1, 2, 3, 4]
+puts rotate_array(x) == [2, 3, 4, 1]   # => true
+puts x == [1, 2, 3, 4]                 # => true
