ca9c17be3de25ae1f672508adce9c44bbd71eabc
DumboCL <wyz513@gmail.com>
2020-04-26 23:05:04 +0800

    finished tic tac toe half way

--- rb101-programming-foundations/lesson-6/tictactoe.rb
+++ rb101-programming-foundations/lesson-6/tictactoe.rb
@@ -6,9 +6,48 @@ COMPUTER_MARKER = 'O'
 WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # cols
                 [[1, 5, 9], [3, 5, 7]]              # diagnals
+VALID_PLAYER = { '1' => 'player',
+                 '2' => 'computer' }
 
 def prompt(msg)
   puts "=> #{msg}"
+end
+
+def clean_screen
+  system "clear"
+end
+
+def first_player_verified?(choice)
+  /^[123]$/.match(choice)
+end
+
+def play_again_veryfied?(choose)
+  /^[YyNn]$/.match(choose)
+end
+
+def choose_first_player
+  choice = nil
+
+  loop do
+    prompt "Welcome to Tic Tac Toe"
+    prompt "Please choose who plays first, this choice will last till the end."
+    prompt "1. You yourself"
+    prompt "2. Computer"
+    prompt "3. Randomly"
+    prompt "Please press 1, 2, or 3, and press Enter"
+    choice = gets.chomp
+    break if first_player_verified?(choice)
+    prompt "Your input is not a valid choice, please choose again."
+  end
+
+  choice
+end
+
+def who_play(choice)
+  if choice == '3'
+    choice = Random.new.rand(1..2).to_s
+  end
+  VALID_PLAYER[choice]
 end
 
 def joinor(elements, separator = ', ', last_conj = 'or')
@@ -22,10 +61,16 @@ def joinor(elements, separator = ', ', last_conj = 'or')
   end
 end
 
-# rubocop:disable Metrics/AbcSize
-def display_board(brd)
-  system "clear"
+# rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+def display_board(brd, scores, first_player)
+  clean_screen
   puts "You're a #{PLAYER_MARKER}. Computer is #{COMPUTER_MARKER}."
+  puts "The first player who reaches 5 wins gets the Final Champion Trophy!"
+  puts "#{first_player.capitalize} plays first."
+  puts "----------------------------"
+  puts "        PLAYER      COMPUTER"
+  puts "Score:       #{scores[:player]}             #{scores[:computer]}"
+  puts "----------------------------"
   puts ""
   puts "     |     |"
   puts "  #{brd[1]}  |  #{brd[2]}  |  #{brd[3]}"
@@ -40,12 +85,16 @@ def display_board(brd)
   puts "     |     |"
   puts ""
 end
-# rubocop:enable Metrics/AbcSize
+# rubocop:enable Metrics/AbcSize, Metrics/MethodLength
 
 def initialize_board
   new_board = {}
   (1..9).each { |num| new_board[num] = INITIAL_MARKER }
   new_board
+end
+
+def initialize_score
+  { player: 0, computer: 0 }
 end
 
 def empty_squares(brd)
@@ -63,8 +112,54 @@ def player_places_piece!(brd)
   brd[square] = PLAYER_MARKER
 end
 
+# if player has 2 squares marked in a row,
+# then computer will defend the 3rd square.
+
+# rubocop:disable Lint/AssignmentInCondition
+def computer_defence(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(PLAYER_MARKER) == 2
+      if empty_index = brd.values_at(*line).index(INITIAL_MARKER)
+        return line[empty_index]
+      else
+        next
+      end
+    end
+  end
+  nil
+end
+
+# if the computer already has 2 in a row,
+# then fill in the 3rd square.
+def computer_offense(brd)
+  WINNING_LINES.each do |line|
+    if brd.values_at(*line).count(COMPUTER_MARKER) == 2
+      if empty_index = brd.values_at(*line).index(INITIAL_MARKER)
+        return line[empty_index]
+      else
+        next
+      end
+    end
+  end
+  nil
+end
+# rubocop:enable Lint/AssignmentInCondition
+
+def square_5_first(brd)
+  if empty_squares(brd).include?(5)
+    return 5
+  end
+  nil
+end
+
 def computer_places_piece!(brd)
-  square = empty_squares(brd).sample
+  if !square = computer_offense(brd)        # 1. offense first
+    if !square = computer_defence(brd)      # 2. defence second
+      if !square = square_5_first(brd)      # 3. square 5 thirdly
+        square = empty_squares(brd).sample  # 4. randomly pick lastly
+      end
+    end
+  end
   brd[square] = COMPUTER_MARKER
 end
 
@@ -79,38 +174,86 @@ end
 def detect_winner(brd)
   WINNING_LINES.each do |line|
     if brd.values_at(line[0], line[1], line[2]).count(PLAYER_MARKER) == 3
-      return 'Player'
+      return 'player'
     elsif brd.values_at(line[0], line[1], line[2]).count(COMPUTER_MARKER) == 3
-      return 'Computer'
+      return 'computer'
     end
   end
   nil
 end
 
-loop do
-  board = initialize_board
+def update_scores(winner, scores)
+  scores[winner.to_sym] += 1
+end
+
+def five_wins?(scores)
+  if scores.key(5)
+    final_winner = scores.key(5).capitalize
+    prompt "#{final_winner} has reached 5 wins first"
+    prompt "#{final_winner} gets the Final Champion Trophy!"
+    true
+  end
+end
+
+def place_piece!(brd, current_player)
+  if current_player == 'player'
+    player_places_piece!(brd)
+  else
+    computer_places_piece!(brd)
+  end
+end
+
+def alternate_player(current_player)
+  current_player == 'player' ? 'computer' : 'player'
+end
+
+def play_again?
+  response = nil
 
   loop do
-    display_board(board)
+    prompt("Play again? (y or n)")
+    response = gets().chomp()
+    if play_again_veryfied?(response)
+      break
+    else
+      prompt('This is not an invalid input, please choose again.')
+    end
+  end
 
-    player_places_piece!(board)
-    break if someone_won?(board) || board_full?(board)
+  play_again = response.downcase() == 'y'
+  play_again
+end
 
-    computer_places_piece!(board)
+# game begins
+scores = initialize_score
+clean_screen
+first_player_choice = choose_first_player
+
+loop do
+  board = initialize_board
+  first_player = current_player = who_play(first_player_choice)
+
+  loop do
+    display_board(board, scores, first_player)
+    place_piece!(board, current_player)
+    current_player = alternate_player(current_player)
     break if someone_won?(board) || board_full?(board)
   end
 
-  display_board(board)
+  display_board(board, scores, first_player)
 
   if someone_won?(board)
-    prompt "#{detect_winner(board)} won!"
+    winner = detect_winner(board)
+    update_scores(winner, scores)
+    display_board(board, scores, first_player)
+    prompt "#{winner.capitalize} won!"
   else
     prompt "It's a tie!"
   end
 
-  prompt "Play again? (y or n)"
-  answer = gets.chomp
-  break unless answer.downcase.start_with?('y')
+  break if five_wins?(scores)
+
+  break unless play_again?
 end
 
 prompt "Thanks for playing Tic Tac Toe! Good bye!"
