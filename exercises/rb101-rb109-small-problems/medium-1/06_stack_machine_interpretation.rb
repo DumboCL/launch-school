556c65ee437f0d4013a3590b7fa2baf510deaf60
DumboCL <wyz513@gmail.com>
2020-06-22 12:50:19 +0800

    interesting text processing stack operation

---
+++ exercises/rb101-rb109-small-problems/medium-1/06_stack_machine_interpretation.rb
@@ -0,0 +1,65 @@
+def is_integer?(str)
+  str.to_i.to_s == str
+end
+
+def minilang(str)
+  stack = []
+  register = 0
+  str.split.each do |operation|
+    if is_integer?(operation)
+      register = operation.to_i
+      next
+    end
+
+    case operation
+    when 'PRINT'
+      puts register
+    when 'PUSH'
+      stack.push(register)
+    when 'POP'
+      register = stack.pop
+    when 'ADD'
+      register += stack.pop
+    when 'SUB'
+      register -= stack.pop
+    when 'MULT'
+      register *= stack.pop
+    when 'DIV'
+      register /= stack.pop
+    when 'MOD'
+      register %= stack.pop
+    end
+  end
+end
+
+# minilang('PRINT')
+# # 0
+
+# minilang('5 PUSH 3 MULT PRINT')
+# # 15
+
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# # 5
+# # 3
+# # 8
+
+# minilang('5 PUSH POP PRINT')
+# # 5
+
+minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# 5
+# 10
+# 4
+# 7
+
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# # 6
+
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# # 12
+
+# minilang('-3 PUSH 5 SUB PRINT')
+# # 8
+
+# minilang('6 PUSH')
+# # (nothing printed; no PRINT commands)
