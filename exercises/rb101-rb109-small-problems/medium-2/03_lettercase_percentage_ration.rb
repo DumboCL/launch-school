8311af3d9a2ff98bcca84c00e50aa80263393d88
DumboCL <wyz513@gmail.com>
2020-06-24 13:18:37 +0800

    add round function on exercise 3

--- exercises/rb101-rb109-small-problems/medium-2/03_lettercase_percentage_ration.rb
+++ exercises/rb101-rb109-small-problems/medium-2/03_lettercase_percentage_ration.rb
@@ -6,7 +6,7 @@ def letter_percentages(str)
   hsh[:neither] = str.chars.count { |chr| chr =~ /[^a-z]/i }
 
   hsh.each do |key, value|
-    hsh[key] = value.to_f / size * 100
+    hsh[key] = (value.to_f / size * 100).round(2)
   end
 
   hsh
@@ -15,3 +15,4 @@ end
 p letter_percentages('abCdef 123') == { lowercase: 50, uppercase: 10, neither: 40 }
 p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25 }
 p letter_percentages('123') == { lowercase: 0, uppercase: 0, neither: 100 }
+p letter_percentages('abcdefGHI') == {:lowercase=>66.67, :uppercase=>33.33, :neither=>0.0}
