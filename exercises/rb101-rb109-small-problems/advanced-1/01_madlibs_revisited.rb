fec7edb9f3e11c021abbc8cef3f51c6e63ec797e
DumboCL <wyz513@gmail.com>
2020-06-25 15:19:08 +0800

    using Kernal#format

---
+++ exercises/rb101-rb109-small-problems/advanced-1/01_madlibs_revisited.rb
@@ -0,0 +1,14 @@
+ADJECTIVES = %w(quick lazy sleepy ugly).freeze
+NOUNS      = %w(fox dog head leg cat tail).freeze
+VERBS      = %w(spins bites licks hurdles).freeze
+ADVERBS    = %w(easily lazily noisly excitedly).freeze
+
+File.open('madlibs.txt') do |file|
+  file.each do |line|
+    puts format(line, noun:      NOUNS.sample,
+                      verb:      VERBS.sample,
+                      adjective: ADJECTIVES.sample,
+                      adverb:    ADVERBS.sample)
+  end
+end
+  
