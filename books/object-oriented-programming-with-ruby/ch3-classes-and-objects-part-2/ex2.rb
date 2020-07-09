f185ba130a725277ce01b802d26394fbf008185b
DumboCL <wyz513@gmail.com>
2020-07-09 12:00:21 +0800

    add customized to_s

---
+++ books/object-oriented-programming-with-ruby/ch3-classes-and-objects-part-2/ex2.rb
@@ -0,0 +1,49 @@
+class MyCar
+  attr_accessor :color, :model
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+
+  def to_s
+    "My car is #{color}, #{year}, #{model}!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+
+lumina.spray_paint('red')
+MyCar.gas_mileage(13, 351)
+puts lumina
