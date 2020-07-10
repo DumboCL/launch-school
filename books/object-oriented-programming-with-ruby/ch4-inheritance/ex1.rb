abfb390ede7b206be26c2ce31d63010a4375a4c4
DumboCL <wyz513@gmail.com>
2020-07-11 02:04:56 +0800

    inheritance exercise

---
+++ books/object-oriented-programming-with-ruby/ch4-inheritance/ex1.rb
@@ -0,0 +1,64 @@
+class Vehicle
+  attr_accessor :color, :model
+  attr_reader :year
+
+  def initialize(year, model, color)
+    @year = year
+    @color = color
+    @model = model
+    @current_speed = 0
+  end
+
+  def speed_up(number)
+    @current_speed += number
+    puts "You push the gas and accelerate #{number} mph."
+  end
+
+  def brake(number)
+    @current_speed -= number
+    puts "You push the brake and decelerate #{number} mph."
+  end
+
+  def current_speed
+    puts "You are now going #{@current_speed} mph."
+  end
+
+  def shut_down
+    @current_speed = 0
+    puts "Let's park this bad boy!"
+  end
+
+  def spray_paint(color)
+    self.color = color
+    puts "Your new #{color} paint job looks great!"
+  end
+
+  def to_s
+    "My vehicle is #{color}, #{year}, #{model}!"
+  end
+
+  def self.gas_mileage(gallons, miles)
+    puts "#{miles / gallons} miles per gallon of gas"
+  end
+end
+
+class MyCar < Vehicle
+  NUMBER_OF_DOORS = 4
+
+  def to_s
+    "My car is #{color}, #{year}, #{model}!"
+  end
+end
+
+class MyTruck < Vehicle
+  NUMBER_OF_DOORS = 2
+
+  def to_s
+    "My truck is #{color}, #{year}, #{model}!"
+  end
+end
+
+lumina = MyCar.new(1997, 'chevy lumina', 'white')
+little_truck = MyTruck.new(2000, 'little truck', 'red')
+puts lumina
+puts little_truck
