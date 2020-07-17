ddcf040fb80aed685c3660b4c47185d05d627b84
DumboCL <wyz513@gmail.com>
2020-07-18 03:31:40 +0800

    finished oo basics inheritance

---
+++ exercises/rb120-object-oriented-programming/oo-basics-inheritance/07_method_lookup_part_1.rb
@@ -0,0 +1,20 @@
+class Animal
+  attr_reader :color
+
+  def initialize(color)
+    @color = color
+  end
+end
+
+class Cat < Animal
+end
+
+class Bird < Animal
+end
+
+cat1 = Cat.new('Black')
+cat1.color
+
+p Cat.ancestors
+
+# [Cat, Animal, Object, Kernel, BasicObject]
