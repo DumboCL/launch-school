d42d55345efc928f3927261c8c10902186267917
DumboCL <wyz513@gmail.com>
2020-08-28 10:11:30 +0800

    learned to use #clone method to protect instance variable

---
+++ exercises/rb120-object-oriented-programming/medium-1/fixed_array.rb
@@ -0,0 +1,68 @@
+class FixedArray
+  def initialize(len)
+    @array = Array.new(len)
+  end
+
+  def [](pos)
+    raise IndexError if pos >= @array.size
+    @array[pos]
+  end
+
+  def to_a
+    @array.clone
+  end
+
+  def []=(pos, ele)
+    raise IndexError if pos >= @array.size
+    @array[pos] = ele
+  end
+
+  def to_s
+    to_a.to_s
+  end
+end
+
+fixed_array = FixedArray.new(5)
+puts fixed_array[3] == nil
+puts fixed_array.to_a == [nil] * 5
+
+fixed_array[3] = 'a'
+puts fixed_array[3] == 'a'
+puts fixed_array.to_a == [nil, nil, nil, 'a', nil]
+
+fixed_array[1] = 'b'
+puts fixed_array[1] == 'b'
+puts fixed_array.to_a == [nil, 'b', nil, 'a', nil]
+
+fixed_array[1] = 'c'
+puts fixed_array[1] == 'c'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', nil]
+
+fixed_array[4] = 'd'
+puts fixed_array[4] == 'd'
+puts fixed_array.to_a == [nil, 'c', nil, 'a', 'd']
+puts fixed_array.to_s == '[nil, "c", nil, "a", "d"]'
+
+puts fixed_array[-1] == 'd'
+puts fixed_array[-4] == 'c'
+
+begin
+  fixed_array[6]
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[-7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
+
+begin
+  fixed_array[7] = 3
+  puts false
+rescue IndexError
+  puts true
+end
