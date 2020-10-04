974ff13ab45f872778a08c5407406cc6b7ab174f
DumboCL <wyz513@gmail.com>
2020-10-04 21:27:56 +0800

    card 2 exercise

---
+++ exercises/rb120-object-oriented-programming/medium-1/deck_of_cards.rb
@@ -0,0 +1,63 @@
+class Card
+  include Comparable
+  attr_reader :rank, :suit
+
+  VALUES = { 'Jack' => 11, 'Queen' => 12, 'King' => 13, 'Ace' => 14 }
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
+  def to_s
+    "#{rank} of #{suit}"
+  end
+
+  def value
+    VALUES.fetch(rank, rank)
+  end
+
+  def <=>(other_card)
+    value <=> other_card.value
+  end
+end
+
+class Deck
+  RANKS = ((2..10).to_a + %w(Jack Queen King Ace)).freeze
+  SUITS = %w(Hearts Clubs Diamonds Spades).freeze
+
+  def initialize
+    @cards = []
+    shuffle_cards
+  end
+
+  def draw
+    card = @cards.shift
+    if @cards.size == 0
+      shuffle_cards
+    end
+
+    card
+  end
+
+  private
+
+  def shuffle_cards
+    RANKS.each do |rank|
+      SUITS.each do |suit|
+        @cards << Card.new(rank, suit)
+      end
+    end
+    @cards.shuffle!
+  end
+end
+
+deck = Deck.new
+drawn = []
+52.times { drawn << deck.draw }
+p drawn.count { |card| card.rank == 5 } == 4
+p drawn.count { |card| card.suit == 'Hearts' } == 13
+
+drawn2 = []
+52.times { drawn2 << deck.draw }
+p drawn != drawn2 # Almost always.
