2632b72dd1a320f497e6c7423b2ab302315977d8
DumboCL <wyz513@gmail.com>
2020-10-30 15:30:33 +0800

    finding missing

---
+++ exercises/rb130-ruby-foundations-more-topics/easy-1/find_missing_numbers.rb
@@ -0,0 +1,8 @@
+def missing(arr)
+  (arr[0]..arr[-1]).to_a - arr
+end
+
+p missing([-3, -2, 1, 5]) == [-1, 0, 2, 3, 4]
+p missing([1, 2, 3, 4]) == []
+p missing([1, 5]) == [2, 3, 4]
+p missing([6]) == []
