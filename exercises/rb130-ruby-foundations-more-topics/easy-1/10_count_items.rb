3850f27576b55de22dbad34ed5cacad04ab247fb
DumboCL <wyz513@gmail.com>
2020-10-30 23:14:08 +0800

    customised count

---
+++ exercises/rb130-ruby-foundations-more-topics/easy-1/10_count_items.rb
@@ -0,0 +1,15 @@
+def count(collection)
+  count = 0
+  collection.each do |ele|
+    count += 1 if yield(ele)
+  end
+
+  count
+end
+
+p count([1,2,3,4,5]) { |value| value.odd? } == 3
+p count([1,2,3,4,5]) { |value| value % 3 == 1 } == 2
+p count([1,2,3,4,5]) { |value| true } == 5
+p count([1,2,3,4,5]) { |value| false } == 0
+p count([]) { |value| value.even? } == 0
+p count(%w(Four score and seven)) { |value| value.size == 5 } == 2
