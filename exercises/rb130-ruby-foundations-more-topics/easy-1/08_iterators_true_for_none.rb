318e94c5cc07408a80dd0c276ccd5e5bf8531316
DumboCL <wyz513@gmail.com>
2020-10-30 21:26:33 +0800

    customised one

---
+++ exercises/rb130-ruby-foundations-more-topics/easy-1/08_iterators_true_for_none.rb
@@ -0,0 +1,18 @@
+def any?(collection)
+  collection.each { |item| return true if yield(item) }
+  false
+end
+
+def none?(collection, &block)
+  !any?(collection, &block)
+end
+
+
+
+p none?([1, 3, 5, 6]) { |value| value.even? } == false
+p none?([1, 3, 5, 7]) { |value| value.even? } == true
+p none?([2, 4, 6, 8]) { |value| value.odd? } == true
+p none?([1, 3, 5, 7]) { |value| value % 5 == 0 } == false
+p none?([1, 3, 5, 7]) { |value| true } == false
+p none?([1, 3, 5, 7]) { |value| false } == true
+p none?([]) { |value| true } == true
