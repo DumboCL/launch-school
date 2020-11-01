5a9160061f35b02b02dd98c9cafd306ce5b14e22
DumboCL <wyz513@gmail.com>
2020-11-01 19:04:10 +0800

    customised map

---
+++ exercises/rb130-ruby-foundations-more-topics/easy-2/03_map.rb
@@ -0,0 +1,10 @@
+def map(arr)
+  arr.each_with_object([]) do |ele, map_arr|
+    map_arr << yield(ele)
+  end
+end
+p map([1, 3, 6]) { |value| value**2 } == [1, 9, 36]
+p map([]) { |value| true } == []
+p map(['a', 'b', 'c', 'd']) { |value| false } == [false, false, false, false]
+p map(['a', 'b', 'c', 'd']) { |value| value.upcase } == ['A', 'B', 'C', 'D']
+p map([1, 3, 4]) { |value| (1..value).to_a } == [[1], [1, 2, 3], [1, 2, 3, 4]]
