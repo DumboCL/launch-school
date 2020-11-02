6b90fbc524ece82a4d26f372ee27b1c658abf25e
DumboCL <wyz513@gmail.com>
2020-11-02 18:08:40 +0800

    each_cons part1

---
+++ exercises/rb130-ruby-foundations-more-topics/easy-2/09_each_cons_part1.rb
@@ -0,0 +1,30 @@
+def each_cons(arr)
+  return nil if arr.size < 2
+
+  (0..(arr.size - 2)).each do |n|
+    yield(arr[n], arr[n + 1])
+  end
+
+  nil
+end
+
+hash = {}
+result = each_cons([1, 3, 6, 10]) do |value1, value2|
+  hash[value1] = value2
+end
+p result == nil
+p hash == { 1 => 3, 3 => 6, 6 => 10 }
+
+hash = {}
+result = each_cons([]) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {}
+p result == nil
+
+hash = {}
+result = each_cons(['a', 'b']) do |value1, value2|
+  hash[value1] = value2
+end
+p hash == {'a' => 'b'}
+p result == nil
