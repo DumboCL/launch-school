70fcdf14664d6b68fdde26ae712e146879e7759d
DumboCL <wyz513@gmail.com>
2021-02-01 11:41:06 +0800

    custome set

---
+++ exercises/ruby-new-challenges/medium/06-custom-set/06_custom_set.rb
@@ -0,0 +1,68 @@
+class CustomSet
+  attr_reader :elements
+
+  def initialize(arr = [])
+    @elements = arr
+    data_process
+  end
+
+  def empty?
+    @elements.size.zero?
+  end
+
+  def contains?(element)
+    @elements.include?(element)
+  end
+
+  def subset?(custom_set)
+    elements.all? { |element| custom_set.contains?(element) }
+  end
+
+  def disjoint?(custom_set)
+    elements.none? { |element| custom_set.contains?(element) }
+  end
+
+  def eql?(other)
+    other.elements.eql?(elements)
+  end
+
+  def add(element)
+    @elements << element
+    data_process
+
+    self
+  end
+
+  def intersection(other)
+    intersection = @elements.select do |element|
+      other.contains?(element)
+    end
+
+    CustomSet.new(intersection)
+  end
+
+  def difference(other)
+    difference = @elements.reject do |element|
+      other.contains?(element)
+    end
+
+    CustomSet.new(difference)
+  end
+
+  def union(other)
+    CustomSet.new(@elements + other.elements)
+  end
+
+  def ==(other)
+    eql?(other)
+  end
+
+  private
+
+  def data_process
+    @elements.uniq!
+    @elements.sort!
+  end
+end
+
+# p CustomSet.new.empty?
