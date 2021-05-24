dd7c682e8d4210e660d1c5ccad20cc0b037d9229
DumboCL <wyz513@gmail.com>
2021-05-24 11:03:04 +0800

    commit book viewer app

--- courses/rb175-networked-applications/working-with-sinatra/book_viewer/book_viewer.rb
+++ courses/rb175-networked-applications/working-with-sinatra/book_viewer/book_viewer.rb
@@ -1,6 +1,20 @@
-require "sinatra"
-require "sinatra/reloader"
+# frozen_string_literal: true
 
-get "/" do
-  File.read "public/template.html"
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
+
+get '/' do
+  @title = 'The Adventures of Sherlock Holmes'
+  @contents = File.readlines('data/toc.txt')
+
+  erb :home
+end
+
+get '/chapters/1' do
+  @title = 'Chapter 1'
+  @contents = File.readlines('data/toc.txt')
+  @chapter = File.read('data/chp1.txt')
+
+  erb :chapter
 end
